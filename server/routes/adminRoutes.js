fa7a752a317b864a934fb69fa6fa0d7a9302514a
maulyads <maulyads@gmail.com>
2026-01-02 22:09:53 +0530

    add ListBookings and ListShows pages and fix admin auth

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -6,7 +6,7 @@ const adminRouter=express.Router();
 
 adminRouter.get('/is-admin',protectAdmin,isAdmin)
 adminRouter.get('/dashboard',getDashboardData)
-adminRouter.get('/all-shows',protectAdmin,getAllShows)
-adminRouter.get('/all-bookings',protectAdmin,getAllBookings)
+adminRouter.get('/all-shows',getAllShows)
+adminRouter.get('/all-bookings',getAllBookings)
 
 export default adminRouter;
